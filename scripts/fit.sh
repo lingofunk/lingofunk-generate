3325c424e2aec330b0dd00cd4f9a27e8ac086d1f
maker <maker@funky.europe-west1-b.c.lingofunk.internal>
2019-02-08 06:21:18 +0000

    update default fit.sh

--- scripts/fit.sh
+++ scripts/fit.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-python ./lingofunc_generate/fit.py \
-    --debug --nrows 100 \
-    --data-path './data/review.csv' \
+python ./lingofunk_generate/fit.py \
+    --data-path './data/reviews.csv' \
     --text-col 'text' \
     --label-col 'stars' \
     --labels-positive 5 --labels-positive 4 \
     --labels-negative 1 --labels-negative 2 \
     --labels-neutral 3 \
-    --new-model \
-    --num-epochs 500
-
+    --num-epochs 1 \
+    --debug --nrows 20000 \
+    --gen-epochs 1
 # --word-level
