f18f7702b801eaadf5266b01aa472ee1a1167107
Vasiliy Alekseev <alvasian@yandex.ru>
2019-02-09 10:52:27 +0300

    fulfil

--- scripts/fit.sh
+++ scripts/fit.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 python ./lingofunk_generate/fit.py \
-    --debug --nrows 100 \
-    --data-path './data/review.csv' \
+    --nrows 50000 \
+    --max-texts-per-label 3000 \
+    --data-path './data/reviews.csv' \
     --text-col 'text' \
     --label-col 'stars' \
     --labels-positive 5 --labels-positive 4 \
     --labels-negative 1 --labels-negative 2 \
     --labels-neutral 3 \
-    --new-model \
     --num-epochs 500
 
+# --new-model
 # --word-level
+# --max-length 10
+# --nrows 50000 \
+# --max-texts-per-label 2000 \
